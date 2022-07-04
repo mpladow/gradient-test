fe9c888a0da41e383d09ebaacb99c0ef6efe2c9a
Michael <mpladow@gmail.com>
2022-07-04 11:56:17 +1000

    updated gradient component with dynamic directions WIP

---
+++ App/Components/Atoms/CButton.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const CButton = () => {
+  return (
+	 <View>
+		<Text>Custom Button</Text>
+	 </View>
+  )
+}
+
+export default CButton
+
+const styles = StyleSheet.create({})
--- App/Components/Molecules/GradientAnimatedQuick.tsx
+++ App/Components/Molecules/GradientAnimatedQuick.tsx
@@ -13,36 +13,95 @@ import { gradient_colours } from '../../constants/theme';
 let timeout = undefined;
 
 const GradientAnimatedQuick = () => {
+	//CONST
 	const COLOURS = ['#00BF6F', '#FDDA25', '#71DBD4', '#00BF6F'];
-
 	const GRADIENT_POINTS = [0, 0.5, 1, 1];
 	const MOVEMENT = GRADIENT_POINTS[1] / 360; // speed of movement
 	const SPEED = 30; // updates every 30ms
 
+	const START_COORD = {x: 0.4, y: 0 }
+	const END_COORD = { x: 0.6, y: 1 };
 	// to be used for dynamic directio of gradient
-	const [startCoord, setStartCoord] = useState({ x: 0.1, y: 0 });
-	const [endCoord, setEndCoord] = useState({ x: 0.8, y: 1 });
 
-	
+
+	let ascXVar = true;
+	const [ascX, setAscX] = useState(true);
 	// set default state
 	const [gradientOptions, setGradientOptions] = useState({
 		colours: COLOURS,
 		gradientLocations: GRADIENT_POINTS,
-		start: startCoord,
-		end: endCoord,
+		start: START_COORD,
+		end: END_COORD,
 	});
 
-	// need to use useRef as we will be using setTimeouts to move the gradient along.
-	// using useRef will allow us to access the updated state
-
+	
+	// using useRef will allow us use the persistent state prior to rerendering
 	const gradientOptionsRef = useRef(gradientOptions);
 	gradientOptionsRef.current = gradientOptions;
 
-	const startRef = useRef(startCoord);
 
 	useEffect(() => {
+		// animateGradientWithState();
 		animateGradient();
 	}, []);
+
+	// can't use state as the state, try using useRef
+	// let animateGradientWithState = () => {
+	// 	if (
+	// 		gradientOptions.gradientLocations[1] -
+	// 			MOVEMENT <=
+	// 		0
+	// 	) {
+	// 		// Shift colours and reset locations
+	// 		console.log(gradientOptions.colours);
+	// 		let gradientColors = [...gradientOptions.colours];
+	// 		let gradientBeingReplaced = gradientColors[1];
+	// 		gradientColors.shift();
+	// 		gradientColors.push(gradientBeingReplaced);
+	// 		// console.log(gradientColors, 'COLOURS')
+
+	// 		// added code to prepare for dynamically setting direction
+	// 		let newStartX = gradientOptions.start.x;
+
+	// 		setGradientOptions({
+	// 			colours: gradientColors,
+	// 			gradientLocations: GRADIENT_POINTS,
+	// 			start: { x: newStartX, y: 0 },
+	// 			end: endCoord,
+	// 		});
+	// 	} else {
+	// 		let updatedLocations =
+	// 			gradientOptions.gradientLocations.map(
+	// 				(item, index) => {
+	// 					if (
+	// 						index ===
+	// 						gradientOptions
+	// 							.gradientLocations
+	// 							.length -
+	// 							1
+	// 					) {
+	// 						return 1;
+	// 					}
+
+	// 					return parseFloat(
+	// 						Math.max(
+	// 							0,
+	// 							item - MOVEMENT
+	// 						).toFixed(4)
+	// 					);
+	// 				}
+	// 			);
+
+	// 		setGradientOptions({
+	// 			colours: [...gradientOptions.colours],
+	// 			gradientLocations: updatedLocations,
+	// 			start: startCoord,
+	// 			end: endCoord,
+	// 		});
+	// 	}
+
+	// 	timeout = setTimeout(animateGradientWithState, SPEED);
+	// };
 
 	let animateGradient = () => {
 		if (
@@ -51,6 +110,8 @@ const GradientAnimatedQuick = () => {
 			0
 		) {
 			// Shift colours and reset locations
+			const directionShiftSpeed = 0.1;
+			console.log(directionShiftSpeed);
 			console.log(gradientOptionsRef.current.colours);
 			let gradientColors = [
 				...gradientOptionsRef.current.colours,
@@ -60,13 +121,44 @@ const GradientAnimatedQuick = () => {
 			gradientColors.push(gradientBeingReplaced);
 			// console.log(gradientColors, 'COLOURS')
 
-			let newStartX = gradientOptionsRef.current.start.x;
+			// dynamic direction change - WIP, still buggy
+			console.log('updating direction ')
+			console.log(gradientOptionsRef.current.start);
+						console.log(
+							gradientOptionsRef
+								.current.end
+						);
+
+			let start = gradientOptionsRef.current.start;
+			let startX = start.x + directionShiftSpeed;
+			startX =
+				ascXVar == true
+					? start.x + directionShiftSpeed
+					: start.x - directionShiftSpeed;
+			if (startX >= 1) {
+				console.log(ascXVar);
+				ascXVar = false;
+			}
+			if (startX <= 0) {
+				console.log(ascXVar);
+				ascXVar = true;
+			}
+			let stop = gradientOptionsRef.current.start;
+			let stopX = start.x + directionShiftSpeed;
+			stopX = ascXVar == true ? stop.x - directionShiftSpeed : stop.x + directionShiftSpeed;
+			//end
 
 			setGradientOptions({
 				colours: gradientColors,
 				gradientLocations: GRADIENT_POINTS,
-				start: { x: newStartX, y: 0 },
-				end: endCoord,
+				start: {
+					x: startX,
+					y: gradientOptionsRef.current.start.y,
+				},
+				end: {
+					x: stopX,
+					y: gradientOptionsRef.current.end.y,
+				},
 			});
 		} else {
 			let updatedLocations =
@@ -92,18 +184,46 @@ const GradientAnimatedQuick = () => {
 					}
 				);
 
+
+
 			setGradientOptions({
 				colours: [
 					...gradientOptionsRef.current.colours,
 				],
 				gradientLocations: updatedLocations,
-				start: startCoord,
-				end: endCoord,
+				start: gradientOptionsRef.current.start,
+				end: gradientOptionsRef.current.end,
 			});
 		}
 
 		timeout = setTimeout(animateGradient, SPEED);
 	};
+
+
+	// const updateDirection = () => {
+	// 	console.log('updating direction');
+	// 		let start = gradientOptionsRef.current.start;
+	// 		let startX = start.x + 0.1;
+	// 		startX =
+	// 			ascXVar == true ? start.x + 0.1 : start.x - 0.1;
+	// 		if (startX >= 1) {
+	// 			console.log(ascXVar);
+	// 			ascXVar = false;
+	// 		}
+	// 		if (startX <= 0) {
+	// 			console.log(ascXVar);
+	// 			ascXVar = false;
+	// 		}
+	// 		let stop = gradientOptionsRef.current.start;
+	// 		let stopX = start.x + 0.1;
+	// 		stopX = ascXVar == true ? stop.x - 0.1 : stop.x + 0.1;
+	// 		if (stopX >= 1) {
+	// 			ascXVar = false;
+	// 		}
+	// 		if (stopX <= 0) {
+	// 			ascXVar = true;
+	// 		}
+	// };
 
 	return (
 		<LinearGradient
